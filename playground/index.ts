bcf57839582e5c30cedcb610c1631ee443c4373c
Bowen7 <zwxdyx@foxmail.com>
2024-01-17 23:08:06 +0800

    use official toml tests

--- playground/index.ts
+++ playground/index.ts
@@ -1,4 +1,4 @@
-import { parseTOML } from "../tests/toml";
+import { parseTOML } from "../examples/toml";
 console.log(
   parseTOML(
     `
--- tests/toml/valid/empty-file.test.ts
+++
@@ -1,6 +0,0 @@
-import { expect, test, describe } from "vitest";
-import { parseTOML } from "../index";
-test("empty file", () => {
-  const toml = ``;
-  expect(parseTOML(toml)).toEqual({});
-});
